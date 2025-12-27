3f86d010915d9daa82809e48ad2c6def37f9f17c
Nguyen Hoang Huy <hhuy3015@gmail.com>
2025-12-27 14:23:38 +0700

    luu code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'category_id',
         'name',
@@ -21,14 +21,26 @@ class Product extends Model
         'description',
         'attributes',
         'view_count',
+        'is_active',
     ];
 
-    protected $cats =[
+    /**
+     * Cast database fields
+     */
+    protected $casts = [
         'attributes' => 'array',
+        'price' => 'float',
+        'sale_price' => 'float',
+        'stock_qty' => 'integer',
+        'view_count' => 'integer',
+        'is_active' => 'boolean',
     ];
 
+    /**
+     * Product belongs to Category
+     */
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class); 
+        return $this->belongsTo(Category::class);
     }
 }
