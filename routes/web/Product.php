efe37d2959662e9c6f54608beb2934263312b62a
Nguyen Hoang Huy <hhuy3015@gmail.com>
2026-01-15 09:50:19 +0700

    chore

--- routes/web/Product.php
+++ routes/web/Product.php
@@ -10,3 +10,12 @@ use App\Http\Controllers\ProductController;
     Route::get('/products', [ProductController::class, 'index'])->name('products');
 
     Route::get('contact', [ProductController::class, 'show'])->name('contact');
+
+
+    // Danh sách sản phẩm
+    Route::get('/products', [ProductController::class, 'index'])
+        ->name('products');
+
+    // Chi tiết sản phẩm (slug)
+    Route::get('/products/{slug}', [ProductController::class, 'show'])
+        ->name('products.show');
